2bd3b2a9575aec4537857bfe61516e1bc30ec532
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-10-17 17:38:20 +0530

    updating ./TRN-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,7 +1,7 @@
 def pipelineINIT() {
   stage('Initiate repos') {
     sh 'rm -rf *'
-    git branch: 'main', url: "https://github.com/phani-devops-practice/${COMPONENT}}.git"
+    git branch: 'main', url: "https://github.com/phani-devops-practice/${COMPONENT}.git"
   }
 }
 
