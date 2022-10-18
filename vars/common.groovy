bb370d4c1aa28bccd961b12f2d822dab3a7747f0
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-10-18 07:53:59 +0530

    updating ./TRN-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -50,6 +50,7 @@ def codeChecks() {
       Qualitychecks: {
         withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'pass', usernameVariable: 'user')]) {
           sh "sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.host.url=http://172.31.14.175:9000 -Dsonar.login=${user} -Dsonar.password=${pass}"
+          sh "sonar-quality-gate.sh ${user} ${pass} 172.31.14.175 ${COMPONENT}"
         }
       },
       unitTests: {
