477755f76b0020ba7723b2bf2bced1b1ee1411f0
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-11-01 13:40:12 +0530

    updating ./TRN-jenkins-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,13 +1,16 @@
 def call() {
   node() {
-    stage('Terraform Init') {
-      sh 'terraform init'
-    }
-    stage('Terraform Plan') {
-      sh 'terraform plan'
-    }
-    stage('Terraform Apply') {
-      sh 'terraform apply -auto-approve'
+    ansiColor('xterm') {
+
+      stage('Terraform Init') {
+        sh 'terraform init'
+      }
+      stage('Terraform Plan') {
+        sh 'terraform plan'
+      }
+      stage('Terraform Apply') {
+        sh 'terraform apply -auto-approve'
+      }
     }
   }
 }  
