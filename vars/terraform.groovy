dd2edc41d3aae52e4b97913e405d9e576d69b228
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-11-01 15:55:49 +0530

    updating ./TRN-jenkins-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,15 +1,21 @@
 def call() {
   node() {
+    properties([
+      parameters([
+        choice(choices: ['dev', 'prod'], description: "Choose Environment", name: "ENV"),
+      ]),
+    ])
+
     ansiColor('xterm') {
 
       stage('Terraform Init') {
-        sh 'terraform init'
+        sh 'terraform init -backend-config=env/${ENV}-backend.tfvars'
       }
       stage('Terraform Plan') {
-        sh 'terraform plan'
+        sh 'terraform plan -var-file=env/$[ENV}.tfvars'
       }
       stage('Terraform Apply') {
-        sh 'terraform apply -auto-approve'
+        sh 'terraform apply -auto-approve -var-file=env/$[ENV}.tfvars'
       }
     }
   }
