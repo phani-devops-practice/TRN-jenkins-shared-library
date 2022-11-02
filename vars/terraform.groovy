4aa56719d6e9a6a4ba8ab25f4ccf544a48034488
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-11-02 13:25:56 +0530

    updating ./TRN-jenkins-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -3,6 +3,7 @@ def call() {
     properties([
       parameters([
         choice(choices: ['dev', 'prod'], description: "Choose Environment", name: "ENV"),
+        choice(choices: ['apply', 'destroy'], description: "Choose Action", name: "APPLY"),
       ]),
     ])
     ansiColor('xterm') {
@@ -25,31 +26,3 @@ def call() {
 }
 
 
-//
-//def call() {
-//  node() {
-//    properties([
-//      parameters([
-//        choice(choices: ['dev', 'prod'], description: "Choose Environment", name: "ENV"),
-//      ]),
-//    ])
-//
-//    ansiColor('xterm') {
-//
-//      stage('Code Checkout') {
-//        sh 'rm -rf *'
-//        git branch: 'main', url: 'https://github.com/phani-devops-practice/roboshop-terraform-mutable.git'
-//      }
-//
-//      stage('Terraform Init') {
-//        sh 'terraform init -backend-config=env/${ENV}-backend.tfvars'
-//      }
-//      stage('Terraform Plan') {
-//        sh 'terraform plan -var-file=env/${ENV}.tfvars'
-//      }
-//      stage('Terraform Apply') {
-//        sh 'terraform apply -auto-approve -var-file=env/${ENV}.tfvars'
-//      }
-//    }
-//  }
-//}
