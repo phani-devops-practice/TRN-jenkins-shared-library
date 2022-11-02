6670e46a0dc558b63cfc5a67a1d02aaf0a6f5e57
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-11-02 06:37:14 +0530

    updating ./TRN-jenkins-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,27 +1,27 @@
-def call() {
-  node() {
-    properties([
-      parameters([
-        choice(choices: ['dev', 'prod'], description: "Choose Environment", name: "ENV"),
-      ]),
-    ])
-
-    ansiColor('xterm') {
-
-      stage('Code Checkout') {
-        sh 'rm -rf *'
-        git branch: 'main', url: 'https://github.com/phani-devops-practice/roboshop-terraform-mutable.git'
-      }
-
-      stage('Terraform Init') {
-        sh 'terraform init -backend-config=env/${ENV}-backend.tfvars'
-      }
-      stage('Terraform Plan') {
-        sh 'terraform plan -var-file=env/${ENV}.tfvars'
-      }
-      stage('Terraform Apply') {
-        sh 'terraform apply -auto-approve -var-file=env/${ENV}.tfvars'
-      }
-    }
-  }
-}  
+//def call() {
+//  node() {
+//    properties([
+//      parameters([
+//        choice(choices: ['dev', 'prod'], description: "Choose Environment", name: "ENV"),
+//      ]),
+//    ])
+//
+//    ansiColor('xterm') {
+//
+//      stage('Code Checkout') {
+//        sh 'rm -rf *'
+//        git branch: 'main', url: 'https://github.com/phani-devops-practice/roboshop-terraform-mutable.git'
+//      }
+//
+//      stage('Terraform Init') {
+//        sh 'terraform init -backend-config=env/${ENV}-backend.tfvars'
+//      }
+//      stage('Terraform Plan') {
+//        sh 'terraform plan -var-file=env/${ENV}.tfvars'
+//      }
+//      stage('Terraform Apply') {
+//        sh 'terraform apply -auto-approve -var-file=env/${ENV}.tfvars'
+//      }
+//    }
+//  }
+//}
