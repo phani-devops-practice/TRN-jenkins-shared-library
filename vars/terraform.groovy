5606d3072939255d9c9aa4d6dfe315864b652574
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-11-06 17:04:41 +0530

    updating ./TRN-jenkins-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -11,7 +11,7 @@ def call() {
     ansiColor('xterm') {
 
       stage('code checkout') {
-        sh 'rm -rf *'
+        sh 'find . | sed -e "1d" | xargs rm -rf'
         git branch: 'main', url: 'https://github.com/phani-devops-practice/roboshop-terraform-mutable.git'
       }
 
